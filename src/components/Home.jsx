153dc3076c2c5b62c0a140504b85d6f7474e2ef3
Udhayaraj <udhaya34.raj34@gmail.com>
2024-10-28 12:06:26 +0530

    Join component completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -138,7 +138,7 @@ useEffect(() => {
       setBorder("container border-bottom border-dark-subtile pt-5 pb-5")
     }  else {
       setBgColor('fixed-top bgc');
-      setBorder("container pt-5 pb-5")
+      setBorder("container pt-3 pb-5")
     }
   };
 
@@ -292,7 +292,7 @@ useEffect(() => {
          </div>
          ))}
           </div>
-          <div class="d-flex justify-content-center w-100 gap-3 mt-3 pb-3">
+          <div className="d-flex justify-content-center w-100 gap-3 mt-3 pb-3">
           <button id="btn1" onClick={()=>cardSlide('left')}></button>
           <button id="btn1" onClick={()=>cardSlide('right')}></button>
          </div>
@@ -301,120 +301,120 @@ useEffect(() => {
         </div>
     </section>
     <section className="sixth-sec pt-5 pb-5 d-flex align-items-center justify-content-center w-100">
-    <div class="row w-70 mt-5 mb-5">
-          <img class="col-xxl-4 rounded-circle img-6" src="src/images/6-2.jpg" alt=""/>
-          <div class="col-xxl-5 content-6 text-white">
-            <p class="text-warning mt-1 fn fw-semibold">$6500 to go</p>
-            <h2 class="fn fw-bold ">Don't Be Mean, Think Green!</h2>
+    <div className="row w-70 mt-5 mb-5">
+          <img className="col-xxl-4 rounded-circle img-6" src="src/images/6-2.jpg" alt=""/>
+          <div className="col-xxl-5 content-6 text-white">
+            <p className="text-warning mt-1 fn fw-semibold">$6500 to go</p>
+            <h2 className="fn fw-bold ">Don't Be Mean, Think Green!</h2>
             <p>Lorem ipsum dolor sit amet nsectetur cing elit. Suspe ndisse suscipit sagittis leo sit met entum estibulum dignissim posuere cubilia Curae. Leo sit met entum cubilia Curae.</p>
-            <button type="button" class="btn fw-semibold btn-outline-warning rounded-pill ps-4 pe-4 text-white">
+            <button type="button" className="btn fw-semibold btn-outline-warning rounded-pill ps-4 pe-4 text-white">
               Donate Now
             </button>
           </div>
-          <div class="container col-xxl-3   mt-5 text-center">
-            <div class="circular-progress">
+          <div className="container col-xxl-3   mt-5 text-center">
+            <div className="circular-progress">
               <span>
-                <p class="fs-1 fn fw-bold mb-0">40%</p>
-                <p class="fs-6 ">Donated</p>
+                <p className="fs-1 fn fw-bold mb-0">40%</p>
+                <p className="fs-6 ">Donated</p>
               </span>
             </div>
           </div>
         </div>
     </section>
-    <section class="seventh-sec row w-100 m-0 p-0">
-        <div class="col-lg-6 h-100 m-0 p-0">
-          <div class="w-100 h-50 row m-0 p-0">
-              <div class="col-6 h-100 p-0 puzz-1" >
-                <div class="puzz-11 fm">
-                  <h3 class="fw-bold">Fighting Global Warming</h3>
-                  <p class="fst-italic">Climate</p>
+    <section className="seventh-sec row w-100 m-0 p-0">
+        <div className="col-lg-6 h-100 m-0 p-0">
+          <div className="w-100 h-50 row m-0 p-0">
+              <div className="col-6 h-100 p-0 puzz-1" >
+                <div className="puzz-11 fm">
+                  <h3 className="fw-bold">Fighting Global Warming</h3>
+                  <p className="fst-italic">Climate</p>
                 </div>
               </div>
-              <div class="col-6 h-100 p-0 puzz-2" >
-                <div class="puzz-11 fm">
-                  <h3 class="fw-bold">Must Love Dogs</h3>
-                  <p class="fst-italic">Rescue</p>
+              <div className="col-6 h-100 p-0 puzz-2" >
+                <div className="puzz-11 fm">
+                  <h3 className="fw-bold">Must Love Dogs</h3>
+                  <p className="fst-italic">Rescue</p>
                 </div>
               </div>
           </div>
-          <div class="w-100 h-50 row m-0 p-0">
-              <div class="col-6 h-100 p-0 puzz-3">
-                <div class="puzz-11 fm">
-                  <h3 class="fw-bold">Protecting Our Oceans</h3>
-                  <p class="fst-italic">Oceans</p>
+          <div className="w-100 h-50 row m-0 p-0">
+              <div className="col-6 h-100 p-0 puzz-3">
+                <div className="puzz-11 fm">
+                  <h3 className="fw-bold">Protecting Our Oceans</h3>
+                  <p className="fst-italic">Oceans</p>
                 </div>
               </div>
-              <div class="col-6 h-100 p-0 puzz-4">
-                <div class="puzz-11 fm">
-                  <h3 class="fw-bold">6 Ways You Can Eat Better</h3>
-                  <p class="fst-italic">Food</p>
+              <div className="col-6 h-100 p-0 puzz-4">
+                <div className="puzz-11 fm">
+                  <h3 className="fw-bold">6 Ways You Can Eat Better</h3>
+                  <p className="fst-italic">Food</p>
                 </div>
               </div>
           </div>
         </div>
-          <div class="col-lg-6 h-100 p-0 puzz-5">
-            <div class="puzz-11 fm">
-              <h3 class="fw-bold">Watch For Climate Action</h3>
-              <p class="fst-italic">Save Earth</p>
+          <div className="col-lg-6 h-100 p-0 puzz-5">
+            <div className="puzz-11 fm">
+              <h3 className="fw-bold">Watch For Climate Action</h3>
+              <p className="fst-italic">Save Earth</p>
             </div>
           </div>
       </section>
-      <footer class="footer w-100">
-      <div class="d-flex w-100 justify-content-center align-items-center">
-        <div class="row w-70 border-bottom border-dark-subtle pb-7">
-          <div class="col-xxl-3 col-md-6 mt-2">
+      <footer className="footer w-100">
+      <div className="d-flex w-100 justify-content-center align-items-center">
+        <div className="row w-70 border-bottom border-dark-subtle pb-7">
+          <div className="col-xxl-3 col-md-6 mt-2">
             <img src="src/images/leaf.png" alt=""/>
           </div>
-          <div class="col-xxl-3 col-md-6 mt-2">
-            <h5 class="fw-bold color fm">What We Do</h5>
-            <ul class="p-0 mt-4">
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">Saving the Arctic</a></li>
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">Fighting Global Warming</a></li>
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">Watch For Climate Action</a></li>
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">Protecting Our Ocean</a></li>
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">6 Ways You Can Eat Better</a></li>
+          <div className="col-xxl-3 col-md-6 mt-2">
+            <h5 className="fw-bold color fm">What We Do</h5>
+            <ul className="p-0 mt-4">
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">Saving the Arctic</a></li>
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">Fighting Global Warming</a></li>
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">Watch For Climate Action</a></li>
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">Protecting Our Ocean</a></li>
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">6 Ways You Can Eat Better</a></li>
             </ul>
           </div>
-          <div class="col-xxl-3 col-md-6 mt-2">
-            <h5 class="fw-bold color fm">News & Media</h5>
-            <ul class="p-0 mt-4">
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">Gallery</a></li>
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">Embedded</a></li>
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">Blog Short Code</a></li>
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">Images</a></li>
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">Events</a></li>
+          <div className="col-xxl-3 col-md-6 mt-2">
+            <h5 className="fw-bold color fm">News & Media</h5>
+            <ul className="p-0 mt-4">
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">Gallery</a></li>
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">Embedded</a></li>
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">Blog Short Code</a></li>
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">Images</a></li>
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">Events</a></li>
             </ul>
           </div>
-          <div class="col-xxl-3 col-md-6 mt-2">
-            <h5 class="fw-bold color fm">About Us</h5>
-            <ul class="p-0 mt-4">
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">Join Us</a></li>
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold"></a></li>
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">About Us</a></li>
-              <li class="link"><a href="#" class="fm text-secondary fs-7 fw-bold">Shop</a></li>
+          <div className="col-xxl-3 col-md-6 mt-2">
+            <h5 className="fw-bold color fm">About Us</h5>
+            <ul className="p-0 mt-4">
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">Join Us</a></li>
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold"></a></li>
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">About Us</a></li>
+              <li className="link"><a href="#" className="fm text-secondary fs-7 fw-bold">Shop</a></li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="d-flex row w-100 justify-content-center">
-        <div class="copy d-flex  w-70 justify-content-between">
-          <div class="col-md-8  pt-5 pb-4">
-            <p class="text-secondary"><span class="fw-bold">Privacy Policy</span> / This is a sample website-cmsmasters <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-c-circle" viewBox="0 0 16 16">
+      <div className="d-flex row w-100 justify-content-center">
+        <div className="copy d-flex  w-70 justify-content-between">
+          <div className="col-md-8  pt-5 pb-4">
+            <p className="text-secondary"><span className="fw-bold">Privacy Policy</span> / This is a sample website-cmsmasters <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-c-circle" viewBox="0 0 16 16">
               <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.146 4.992c-1.212 0-1.927.92-1.927 2.502v1.06c0 1.571.703 2.462 1.927 2.462.979 0 1.641-.586 1.729-1.418h1.295v.093c-.1 1.448-1.354 2.467-3.03 2.467-2.091 0-3.269-1.336-3.269-3.603V7.482c0-2.261 1.201-3.638 3.27-3.638 1.681 0 2.935 1.054 3.029 2.572v.088H9.875c-.088-.879-.768-1.512-1.729-1.512"/>
             </svg></span> 2023 / All Rights Reserved</p>
           </div>
-          <div class="col-md-4  pt-5 pb-4">
-            <ul class="d-flex justify-content-end gap-3">
-              <li><a class="text-secondary" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+          <div className="col-md-4  pt-5 pb-4">
+            <ul className="d-flex justify-content-end gap-3">
+              <li><a className="text-secondary" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
                 <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
               </svg></a></li>
-              <li><a class="text-secondary" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+              <li><a className="text-secondary" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter" viewBox="0 0 16 16">
                 <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
               </svg></a></li>
-              <li><a class="text-secondary" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
+              <li><a className="text-secondary" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-youtube" viewBox="0 0 16 16">
                 <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
               </svg></a></li>
-              <li><a class="text-secondary" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+              <li><a className="text-secondary" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16">
                 <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
               </svg></a></li>
             </ul>
