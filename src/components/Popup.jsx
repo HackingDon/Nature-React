3effef869b13bc392c9aabb573aeaa57fa3db772
Udhayaraj <udhaya34.raj34@gmail.com>
2024-11-06 16:48:15 +0530

    refresh message after closing

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -11,7 +11,7 @@ const Popup = (props) => {
     <div>
       <div className="container-fluid position-fixed top-0" style={{height:'100vh',backgroundColor:'rgba(34,49,57,0.9)'}}>
         <div className="d-flex justify-content-end w-100">
-            <button className="btn border-0 text-white fw-bold fs-2 me-4 mt-3" onClick={()=>props.setTrigger(false)}>X</button>
+            <button className="btn border-0 text-white fw-bold fs-2 me-4 mt-3" onClick={()=>{props.setTrigger(false);setText('')}}>X</button>
         </div>
         <div className="h-75 w-100 d-flex flex-column align-items-center justify-content-center ">
         <form className="d-flex gap-4  w-50" onSubmit={()=>submit(Event)}>
