6c2be935013114323eed529e37dd8ce0fc96f72b
Udhayaraj <udhaya34.raj34@gmail.com>
2024-11-13 15:56:59 +0530

    styles are modified

--- src/components/alertmessage.jsx
+++ src/components/alertmessage.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Alertmessage = (props) => {
   return (
     <>
-    {props.alert && <div className="container-fluid position-fixed bottom-0" style={{zIndex:2}}>
+    {props.alert && <div className="container-fluid position-fixed" style={{zIndex:2,top:'10%'}}>
       <div className="d-flex w-100 h-100 justify-content-center align-items-center">
-        <h5 className="text-center position-relative fh text-white bg-secondary p-3 rounded-3" style={{zIndex:'2'}}>{props.message}</h5>
+        <h5 className="text-center position-relative fh text-white bg-dark p-3 rounded-3" style={{zIndex:'2'}}>{props.message}</h5>
       </div>
       </div>}</>
   )
